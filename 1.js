098ca6310b08b50b386b7e209e735f58d784f8ad
Minh Thu <MinhThuMinhThu0205@gmail.com>
2020-06-17 22:20:35 +0700

    update hinh trang CV

---
+++ 1.js
@@ -0,0 +1,15 @@
+﻿ $(function(){
+ 	$('.xuong, .kn').click(function(event) {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:$('#khoiedu').offset().top}, 400);
+ 	});
+ 	$('.sp').click(function(event) {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:$('#khoisp').offset().top}, 400);
+ 	});
+ 	$('.cm').click(function(event) {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:$('#khoicm').offset().top}, 400);
+ 	});
+})  
+ 
